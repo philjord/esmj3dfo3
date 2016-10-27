ac02a34791c3ab640ea24b2f2a8ed63ac556c9f3
phil <philjord@ihug.co.nz>
2016-10-27 18:23:47 +1300

    package javax.vecmath renamed to org.jogamp.vecmath
    To keep in line with Java3D 1.7.0

--- esmj3dfo3/src/esmj3dfo3/j3d/j3drecords/type/J3dCREA.java
+++ esmj3dfo3/src/esmj3dfo3/j3d/j3drecords/type/J3dCREA.java
@@ -3,10 +3,9 @@ package esmj3dfo3.j3d.j3drecords.type;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.vecmath.Color3f;
-
 import org.jogamp.java3d.Transform3D;
 import org.jogamp.java3d.TransformGroup;
+import org.jogamp.vecmath.Color3f;
 
 import esmj3d.data.shared.subrecords.MODL;
 import esmj3d.j3d.BethRenderSettings;
